2f90eec6c0217a5abcbbdff1cdcaed438fdc87e2
Erik Michaels-Ober <sferik@gmail.com>
2011-05-11 14:27:36 -0700

    Add error handling

--- lib/futures_pipeline/connection.rb
+++ lib/futures_pipeline/connection.rb
@@ -7,6 +7,7 @@ module FuturesPipeline
     def connection
       Faraday::Connection.new(:ssl => {:verify => false}, :url => 'http://militarydemo.pipelinenc.com') do |connection|
         connection.use Faraday::Request::UrlEncoded
+        connection.use Faraday::Response::RaiseError
         connection.use Faraday::Response::Rashify
         connection.use Faraday::Response::ParseJson
         connection.adapter(Faraday.default_adapter)
