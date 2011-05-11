18e103b8d3b7a8391d8097a96d91e2d7f2046277
Ryan Resella <ryan@codeforamerica.org>
2011-05-11 11:56:01 -0700

    Added fixtures for career.json and test for career

--- spec/futures_pipeline/client_spec.rb
+++ spec/futures_pipeline/client_spec.rb
@@ -5,9 +5,18 @@ describe FuturesPipeline::Client do
     @client = FuturesPipeline::Client.new
   end
 
-  describe ".careers" do
-    @client = FuturesPipeline::Client.new
-    careers = @client.careers
-    careers.first.title.should == "Chief Executives"
+  describe "#careers" do
+    it "should fetch all careers" do
+      careers = @client.careers
+      careers.first.title.should == "Chief Executives"
+    end
   end
+  
+  describe "#career" do
+    it "should fetch one career" do
+      career = @client.career
+      carrer.title.should == "Chief Executives"
+    end
+  end
+  
 end
