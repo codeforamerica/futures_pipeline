792bb7665ef34e01d77e21356cb02fdd9c17f4fe
Erik Michaels-Ober <sferik@gmail.com>
2011-05-11 14:27:13 -0700

    Add specs for error handling

---
+++ spec/faraday/response_spec.rb
@@ -0,0 +1,28 @@
+require 'helper'
+
+describe Faraday::Response do
+  before do
+    @client = FuturesPipeline::Client.new
+  end
+
+  {
+    400 => Faraday::Error::ClientError,
+    401 => Faraday::Error::ClientError,
+    403 => Faraday::Error::ClientError,
+    404 => Faraday::Error::ResourceNotFound,
+    406 => Faraday::Error::ClientError,
+    500 => Faraday::Error::ClientError,
+    501 => Faraday::Error::ClientError,
+    502 => Faraday::Error::ClientError,
+    503 => Faraday::Error::ClientError,
+  }.each do |status, exception|
+    context "when HTTP status is #{status}" do
+
+      it "should raise #{exception.name} error" do
+        lambda do
+          @client.career("11-1011.01")
+        end.should raise_error(exception)
+      end
+    end
+  end
+end
