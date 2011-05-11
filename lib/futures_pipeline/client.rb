d0d83889e26ed72262e3172229639ba6b6bc4bee
Erik Michaels-Ober <sferik@gmail.com>
2011-05-10 18:55:29 -0700

    Add and mix in Connection class

--- lib/futures_pipeline/client.rb
+++ lib/futures_pipeline/client.rb
@@ -1,4 +1,7 @@
+require 'futures_pipeline/connection'
+
 module FuturesPipeline
   class Client
+    include FuturesPipeline::Connection
   end
 end
---
+++ lib/futures_pipeline/connection.rb
@@ -0,0 +1,16 @@
+require 'faraday_middleware'
+
+module FuturesPipeline
+  module Connection
+    private
+
+    def connection
+      Faraday::Connection.new(:ssl => {:verify => false}, :url => 'http://militarydemo.pipelinenc.com') do |connection|
+        connection.use Faraday::Request::UrlEncoded
+        connection.use Faraday::Response::Rashify
+        connection.use Faraday::Response::ParseJson
+        connection.adapter(Faraday.default_adapter)
+      end
+    end
+  end
+end
