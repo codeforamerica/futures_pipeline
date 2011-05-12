fd5820c1afba56950b88dc3e25f5aa27dc1acc77
Erik Michaels-Ober <sferik@gmail.com>
2011-05-11 17:16:50 -0700

    No need to pass SSL options for HTTP connection

--- lib/futures_pipeline/connection.rb
+++ lib/futures_pipeline/connection.rb
@@ -5,7 +5,7 @@ module FuturesPipeline
     private
 
     def connection
-      Faraday::Connection.new(:ssl => {:verify => false}, :url => 'http://militarydemo.pipelinenc.com') do |connection|
+      Faraday.new(:url => 'http://militarydemo.pipelinenc.com') do |connection|
         connection.use Faraday::Request::UrlEncoded
         connection.use Faraday::Response::RaiseError
         connection.use Faraday::Response::Rashify
