fb57185449b296224bbb49ebdf51d57e5572837a
Erik Michaels-Ober <sferik@gmail.com>
2011-06-23 19:41:02 -0700

    Wrap all module methods in class << self

--- lib/futures_pipeline.rb
+++ lib/futures_pipeline.rb
@@ -1,20 +1,22 @@
 require 'futures_pipeline/client'
 
 module FuturesPipeline
-  # Alias for FuturesPipeline::Client.new
-  #
-  # @return [FuturesPipeline::Client]
-  def self.new
-    FuturesPipeline::Client.new
-  end
+  class << self
+    # Alias for FuturesPipeline::Client.new
+    #
+    # @return [FuturesPipeline::Client]
+    def new
+      FuturesPipeline::Client.new
+    end
 
-  # Delegate to FuturesPipeline::Client
-  def self.method_missing(method, *args, &block)
-    return super unless new.respond_to?(method)
-    new.send(method, *args, &block)
-  end
+    # Delegate to FuturesPipeline::Client
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
 
-  def self.respond_to?(method, include_private=false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
 end
